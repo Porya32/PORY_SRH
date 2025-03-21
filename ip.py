b910443835a6fed23982cc62d0ab5e36e04b4469
Porya32 <119894506+Porya32@users.noreply.github.com>
2025-03-21 14:44:10 +0330

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,34 @@
+import time
+import pyfiglet
+print(f"\033[32m")
+time.sleep(1)
+# کلمه مورد نظر
+text = "D⃞     o⃞     S⃞"
+
+# ایجاد متن بزرگ و افکت دار
+large_text = pyfiglet.figlet_format(text, font="slant")
+
+# چاپ کردن متن بزرگ
+print(large_text)
+
+print(f"\033[32m")
+time.sleep(1)
+# کلمه مورد نظر
+text = "i p  d d o s"
+
+# ایجاد متن بزرگ و افک
+large_text = pyfiglet.figlet_format(text, font="slant")
+
+# چاپ کردن متن بزرگ
+print(large_text)
+
+
+
+time.sleep(1)
+user_input = input("guid target:")
+
+result = f"                                                                         1.523.764.988"
+# نمایش نتیجه
+print(result)
+
+print(f"\033[36m")
